ba884e25febdf99591bf73cf08ecc9fc66ed0545
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2025-01-24 22:10:24 +0300

    Updated Feed On Fri Jan 24 10:10:24 PM EAT 2025

--- update_feeds.sh
+++ update_feeds.sh
@@ -4,6 +4,7 @@ cd ~/feeds/
 ./tinyfeed -i ./inputs.txt -o ./index.html
 
 if [[ $? -eq 0 ]]; then
+    sed -i "/<footer>/a \<p>Feed Updated on $(date)</p>" index.html
     git add .
     git commit -m "Updated Feed On $(date)"
     git push
